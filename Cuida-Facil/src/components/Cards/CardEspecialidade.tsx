a155fa3e2bcb763f171a88bca8da33ecf17e0ddb
= <=>
2025-11-08 09:49:27 -0300

    feat: cards atualizados

--- Cuida-Facil/src/components/Cards/CardEspecialidade.tsx
+++ Cuida-Facil/src/components/Cards/CardEspecialidade.tsx
@@ -1,17 +1,58 @@
-import type { TipoCardEspecialidade } from '../../types/CardTypes';
-import CardBase from './CardBase';
+import type { TipoEspecialidade } from '../../types/TipoEspecialidade';
+import { useAccessibility } from '../../context/AcessibilityContext';
 
-type CardEspecialidadeProps = {
-    card: TipoCardEspecialidade;
-}
+type EspecialidadeCardProps = {
+  especialidade: TipoEspecialidade;
+};
 
-export default function CardEspecialidade({ card }: CardEspecialidadeProps) {
-    return (
-        <CardBase cssClass={`card-especialidades-medicas ${card.cssClass}`}>
-            <div className="card-link">
-                <h2>{card.titulo}</h2>
-                <p>{card.descricao}</p>
-            </div>
-        </CardBase>
-    );
+export default function EspecialidadeCard({ especialidade }: EspecialidadeCardProps) {
+  const { lerTexto, leitorAtivo, pararLeitura } = useAccessibility();
+
+  const gerarCssClass = (nome: string) => {
+    return `card-${nome
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\s+/g, '-')}`;
+  };
+
+  const cssClass = gerarCssClass(especialidade.nome);
+  const imagemUrl = especialidade.urlImagemEspecialidades;
+
+
+  const handleLeituraCard = () => {
+    if (!leitorAtivo) return;
+    
+    const textoResumo = `
+      Especialidade: ${especialidade.nome}. 
+      ${especialidade.descricao ? `Descrição: ${especialidade.descricao}` : 'Sem descrição disponível.'}
+    `;
+    
+    lerTexto(textoResumo);
+  };
+
+  return (
+    <div
+      className={`unidade-card ${cssClass}`}
+      style={imagemUrl ? {
+        backgroundImage: `url(${imagemUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      } : undefined}
+      tabIndex={0}
+      onMouseEnter={handleLeituraCard}
+      onFocus={handleLeituraCard}
+      onMouseLeave={pararLeitura}
+    >
+      <div className="unidade-card-overlay">
+        <div className="especialidade-card-content">
+          <h2 className="unidade-card-title">{especialidade.nome}</h2>
+          {especialidade.descricao && (
+            <p className="especialidade-descricao">{especialidade.descricao}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
